07d2b8e693e706ad489b6aa6f734ef967e8f6b14
KellyPT <trankphuong@gmail.com>
2020-10-10 21:17:28 -0700

    first data model

--- temperature_unit_conversion/src/Model.js
+++ temperature_unit_conversion/src/Model.js
@@ -1,3 +1,9 @@
-const initModel = {};
+const initModel = {
+  leftValue: 0,
+  leftUnit: 'Celsius',
+  rightValue: 32,
+  rightUnit: 'Fahrenheit',
+  sourceLeft: true // determine if user enters initial value on the left or right
+};
 
 export default initModel;
