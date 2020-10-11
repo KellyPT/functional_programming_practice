38f35b13a6d5fd8012fc3554721412f641b6eeb9
KellyPT <trankphuong@gmail.com>
2020-10-10 21:53:18 -0700

    update input values when user interacts with converter

--- temperature_unit_conversion/src/View.js
+++ temperature_unit_conversion/src/View.js
@@ -1,6 +1,7 @@
 import * as R from 'ramda';
 import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
+import { leftValueInputMsg, rightValueInputMsg } from './Update';
 
 const { div, h1, pre, input, select, option } = hh(h);
 
@@ -13,12 +14,13 @@ function unitOptions(selectedUnit) {
   );
 }
 
-function unitSection(dispatch, unit, value) {
+function unitSection(dispatch, unit, value, inputMsg) {
   return div({ className: 'w-50 ma1' }, [
     input({
       type: 'text',
       className: 'db w-100 mv2 pa2 input-reset ba',
-      value
+      value,
+      oninput: (e) => dispatch(inputMsg(e.target.value))
     }),
     select(
       {
@@ -33,8 +35,13 @@ function view(dispatch, model) {
   return div({ className: 'mw6 center' }, [
     h1({ className: 'f2 pv2 bb' }, 'Temperature Unit Converter'),
     div({ className: 'flex' }, [
-      unitSection(dispatch, model.leftUnit, model.leftValue),
-      unitSection(dispatch, model.rightUnit, model.rightValue)
+      unitSection(dispatch, model.leftUnit, model.leftValue, leftValueInputMsg),
+      unitSection(
+        dispatch,
+        model.rightUnit,
+        model.rightValue,
+        rightValueInputMsg
+      )
     ]),
     pre(JSON.stringify(model, null, 2))
   ]);
